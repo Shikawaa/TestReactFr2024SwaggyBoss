1de0de57f2406f7ea7da5ff4a6c224fde1109be3
Arnaud GAY <arnogay@gmail.com>
2024-03-12 16:55:25 +0100

    Import React

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Le code participatif</h1>
+        <p>Alexandre&Arnaud</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
